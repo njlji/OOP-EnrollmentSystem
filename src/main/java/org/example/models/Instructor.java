b3c50e38c5be9d40471fca8bc2e88e105e7e82c6
Wreign Eyriel P. Hizon <wreigneyriel@gmail.com>
2026-03-07 12:01:17 +0800

    inheritance and Tuition Fee

---
+++ src/main/java/org/example/models/Instructor.java
@@ -0,0 +1,6 @@
+package org.example.models;
+
+public class Instructor extends Person{
+    Course courses = new Course();
+
+}
---
+++ src/main/java/org/example/models/Person.java
@@ -0,0 +1,31 @@
+package org.example.models;
+
+public class Person {
+    private String ID;
+    private String name;
+
+    public Person(){
+
+    }
+
+    public Person(String name, String ID){
+        this.name = name;
+        this.ID = ID;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
